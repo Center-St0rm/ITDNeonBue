e792dcc8fce50c264d590f95e020499be9a463f8
Center-St0rm <149319352+Center-St0rm@users.noreply.github.com>
2024-10-27 11:29:25 -0400

    just addded motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ITDBHardware/StudedWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ITDBHardware/StudedWheels.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.teamcode.ITDBHardware;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 public class StudedWheels {
+    public DcMotorEx backLeft;
+    public DcMotorEx frontLeft;
+    public DcMotorEx backRight;
+    public DcMotorEx frontRight;
+    
 }
